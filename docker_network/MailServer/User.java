e56e0853fafa4ca24ff0ec09154375cc067c75c5
Romain Malaise <romainmalaise230105@gmail.com>
2025-11-22 17:23:03 +0100

    User.java fini

--- docker_network/MailServer/User.java
+++ docker_network/MailServer/User.java
@@ -1,2 +1,37 @@
 // vérifier user/password, domaine
 // Dire oui ou non
+
+public class User {
+    private static final String[] allowedUsers = { "dcd@gembloux.uliege.be", "vj@gembloux.uliege.be",
+            "dcd@info.uliege.be",
+            "vj@info.uliege.be", "dcd@uliege.be", "vj@uliege.be" };
+    private String user;
+
+    public User(String user) {
+        this.user = user;
+    }
+
+    public boolean userExists() {
+        for (String email : allowedUsers) {
+            if (this.user.equals(email))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean checkPassword(String password) {
+        return password.equals("password");
+    }
+
+    public boolean checkDomain() {
+        return getUserDomain().equals(MailServer.getDomain());
+    }
+
+    public String getUserDomain() {
+        return user.split("@")[1];
+    }
+
+    public String getUserName() {
+        return user.split("@")[0];
+    }
+}
