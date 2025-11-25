cd00ba41e419d690cc31ec7bd916ef53b55bc245
Baptiste <b.renard@student.uliege.be>
2025-11-25 13:03:04 +0100

    SMTP and POP3

--- docker_network/MailServer/POP3Handler.java
+++ docker_network/MailServer/POP3Handler.java
@@ -4,8 +4,14 @@
  * STAT, LIST, RETR, DELE, QUIT
  * 
  */
+import java.util.List;
 
 public class POP3Handler extends Handler {
+    private int seq = 0;
+    private User user;
+    private Mailbox mailbox;
+    private List<Message> messages;
+    private int totalBytes;
 
     public POP3Handler(ConnectionIO connecionIO) {
         super(connecionIO);
@@ -13,13 +19,111 @@ public class POP3Handler extends Handler {
 
     @Override
     public void sendGreeting() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendGreeting'");
+        connectionIO.writeMessage("+OK POP3 server ready");
     }
 
     @Override
     public void handleCommand(String command) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleCommand'");
+        if(command == null){
+            connectionActive = false;
+            return;
+        }
+        String[] split = command.split("\\s+");
+        switch (split[0]){
+            case "USER":
+                if(split.length < 2 || seq != 0){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                String username = split[1];
+                user = new User(username);
+                if(user.userExists())
+                    connectionIO.writeMessage("+OK");
+                else
+                    connectionIO.writeMessage("BAD");
+
+                seq = 1;
+                return;
+
+            case "PASS":
+                if(seq != 1){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                if(user.checkPassword(split[1]))
+                    connectionIO.writeMessage("+OK Mailbox locked and ready");
+
+                else
+                    connectionIO.writeMessage("BAD");
+
+                seq = 2;
+                mailbox = MailStore.loadMailbox(user, "INBOX");
+                return;
+
+            case "STAT":
+                if(seq != 2){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                totalBytes = 0;
+                for(Message message : messages = mailbox.getAllMessages()){
+                    totalBytes += message.size();
+                }
+                connectionIO.writeMessage("+OK " + messages.size() + " " + totalBytes);
+                seq = 3;
+                return;
+
+            case "LIST":
+                if(seq != 3){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                connectionIO.writeMessage("+OK " + messages.size() + " messages (" + totalBytes + " octects)");
+                for(int i = 0; i < messages.size(); i++){
+                    connectionIO.writeMessage((i+1) + " " + messages.get(i).size());
+                }
+                connectionIO.writeMessage(".");
+                seq = 4;
+                return;
+
+            case "RETR":
+                if(seq != 4 || split.length < 2){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                int ref = Integer.parseInt(split[1]);
+                Message msg = messages.get(ref-1);
+                if(msg == null){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                for(String line : msg.getDataLines())
+                    connectionIO.writeMessage(line);
+
+                connectionIO.writeMessage(".");
+                return;
+
+            case "DELE":
+                if(seq != 4 || split.length < 2){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                int ref2 = Integer.parseInt(split[1]);
+                Message msg2 = messages.get(ref2-1);
+                if(msg2 == null){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                msg2.addFlag("\\Deleted");
+                connectionIO.writeMessage("+OK Message marked for deletion");
+                return;
+
+            case "QUIT":
+                mailbox.expunge();
+                MailStore.expungeMailbox(user, "INBOX", mailbox);
+                connectionIO.writeMessage("+OK Goodbye");
+                connectionActive = false;
+                return;
+        }
     }
 }
--- docker_network/MailServer/SMTPHandler.java
+++ docker_network/MailServer/SMTPHandler.java
@@ -2,8 +2,15 @@
  * détecte une commande, exécuter ce qu'elle doit faire, envoyer réponse
  * HELO, MAIL FROM, RCPT TO, DATA
  */
+import java.util.ArrayList;
 
 public class SMTPHandler extends Handler {
+    private boolean ready;
+    private String clientDomain;
+    private String from;
+    private ArrayList<String> rcpts;
+    private boolean reading;
+    private Message currentMsg;
 
     public SMTPHandler(ConnectionIO connecionIO) {
         super(connecionIO);
@@ -11,13 +18,113 @@ public class SMTPHandler extends Handler {
 
     @Override
     public void sendGreeting() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendGreeting'");
+        connectionIO.writeMessage("220 " + MailServer.getDomain() + " Service ready");
     }
 
     @Override
     public void handleCommand(String command) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleCommand'");
+        if(command == null){
+            connectionActive = false;
+            return;
+        }
+
+        String[] split = command.split("\\s+");
+        if(split.length < 1){
+            connectionIO.writeMessage("BAD");
+            return;
+        }
+
+        switch(split[0]){
+            case "HELO":
+                if (split.length < 2){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                ready = true;
+                clientDomain = split[1];
+                connectionIO.writeMessage("250 " + MailServer.getDomain() + " greets " + clientDomain);
+                return;
+
+            case "DATA":
+                if(from == null || rcpts == null || rcpts.isEmpty()){
+                    connectionIO.writeMessage("BAD");
+                    return;
+                }
+                connectionIO.writeMessage("354 End data with <CRLF>.<CRLF>");
+                currentMsg = new Message();
+                currentMsg.setFrom(from);
+                for(String rcpt : rcpts){
+                    currentMsg.addRcpt(rcpt);
+                }
+                reading = true;
+                return;
+
+            case "QUIT":
+                connectionIO.writeMessage("221 Bye");
+                connectionActive = false;
+                return;
+        }
+
+        if(command.startsWith("MAIL FROM:")){
+            if(!ready){
+                connectionIO.writeMessage("BAD");
+                return;
+            }
+            from = command.substring(command.indexOf(":")+1);
+            if (from == null){
+                connectionIO.writeMessage("BAD");
+                return;
+            }
+            connectionIO.writeMessage("250 OK");
+            rcpts = new ArrayList<>();
+            return;
+        }
+        if(command.startsWith("RCPT TO:")){
+            if(from == null){
+                connectionIO.writeMessage("BAD");
+                return;
+            }
+            String rcpt = command.substring(command.indexOf(":")+1);
+            if (rcpt == null){
+                connectionIO.writeMessage("BAD");
+                return;
+            }
+            rcpts.add(rcpt);
+            connectionIO.writeMessage("250 OK");
+            return;
+        }
+        if(reading){
+            if(command.equals(".")){
+                sendMessage(currentMsg);
+                reading = false;
+                from = null;
+                rcpts = null;
+                currentMsg = null;
+                return;
+            }
+            currentMsg.addDataLine(command);
+            return;
+        }
+        connectionIO.writeMessage("BAD");
+        return;
+    }
+
+    private void sendMessage(Message msg){
+        for(String rcpt : rcpts){
+            User user = new User(rcpt);
+            String domain = user.getUserDomain();
+            if(domain == null)
+                continue;
+
+            if(MailServer.getDomain().equals(domain)){
+                Mailbox userMailbox = MailStore.loadMailbox(user, "INBOX");
+                userMailbox.addMessage(msg);
+                MailStore.saveMailbox(user, "INBOX", userMailbox);
+            }
+            else{
+                SMTPTransmitter ts = new SMTPTransmitter(domain);
+                ts.forward(msg, rcpt);
+            }
+        }
     }
 }
